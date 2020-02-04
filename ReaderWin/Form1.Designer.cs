52ae53bae4679e3e1a3b63cb0c86e93c65178c42
Maxwellsonleison <31752207+Maxwellsonleison@users.noreply.github.com>
2020-02-04 19:09:04 +0800

    Update
    上传项目文件

---
+++ ReaderWin/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ReaderWin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vScrollBar1.Location = new System.Drawing.Point(781, -1);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(21, 464);
+            this.vScrollBar1.TabIndex = 0;
+            this.vScrollBar1.ValueChanged += new System.EventHandler(this.vScrollBar1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.CausesValidation = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("宋体", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Location = new System.Drawing.Point(12, 43);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(225, 329);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.Visible = false;
+            this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_Click);
+            // 
+            // button2
+            // 
+            this.button2.CausesValidation = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("宋体", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(12, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 25);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "打开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 464);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Reader";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ReaderWin/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace ReaderWin
+{
+    public partial class Form1 : Form
+    {
+        public string ContentF;
+        public string TitleF;
+        //set scroll speed
+        public int ScrollSpeed = 60;
+        //save last scroll local
+        float ScrollCurrentPosition=0f;
+        NovelsParser.OneChapter[] oneChapters;
+        public Form1()
+        {
+            InitializeComponent();
+            //using doublebuffer to refuse flash
+            DoubleBuffered = true;
+            
+        }
+
+        private void GetNovel(int index)
+        {
+            if (oneChapters == null || oneChapters.Length == 0) return;
+            var t = oneChapters;
+            TitleF = t[index].Title;
+            ContentF = t[index].Content;
+        }
+
+        #region FormMethod
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TitleF)) return;
+            base.OnPaint(e);
+            e.Graphics.PageUnit = GraphicsUnit.Pixel;
+            e.Graphics.TranslateTransform(0, 0);
+            e.Graphics.TranslateTransform(0, ScrollCurrentPosition);
+            DrawTitle(TitleF, new Font("微软雅黑", 20, FontStyle.Bold), e.Graphics, new SolidBrush(Color.Black));
+            DrawContent(ContentF, new Font("幼圆", 10, FontStyle.Regular), e.Graphics, new SolidBrush(Color.Black));
+            
+        }
+        #region DrawMethod
+        private void DrawTitle(string str, Font f, Graphics e, Brush b)
+        {
+            int leftsite = this.Width / 5;
+            e.DrawString(str, f, b, new Point(leftsite, 0));
+
+        }
+        private void DrawContent(string str, Font f, Graphics e, Brush b)
+        {
+            int startside = this.Width / 5;
+            int starthigh = 50;
+            StringFormat stf = new StringFormat();
+            Size sf = new Size(this.Width - 2 * this.Width / 5, this.Height);
+            stf.Alignment = StringAlignment.Near;
+            stf.LineAlignment = StringAlignment.Near;
+            var s = TextRenderer.MeasureText(e, str, f, sf, TextFormatFlags.NoPadding);
+            s.Width = this.Width - startside * 2;
+            
+            s.Height += 1000;
+            e.SmoothingMode = SmoothingMode.AntiAlias;
+            e.InterpolationMode = InterpolationMode.NearestNeighbor;
+            e.DrawString(str, f, b, new Rectangle(new Point(startside, starthigh), s));
+            if (s.Height > sf.Height) { vScrollBar1.Maximum = s.Height/10 - sf.Height/10; vScrollBar1.Minimum = 0; }
+        }
+        #endregion
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void ScrollToLocal(float position,bool canScr)
+        {
+
+            if (position>=vScrollBar1.Maximum) return;
+            if(canScr)
+            {
+                ScrollCurrentPosition = -position*10;
+                this.Invalidate();
+            }
+        }
+
+        Point TreeviewScrolPos;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TreeviewScrolPos = new Point(0,0);
+            if (treeView1.Visible)
+            {
+                TreeviewScrolPos = treeView1.AutoScrollOffset;
+                treeView1.Visible = false;
+            }
+            else
+            {
+                treeView1.Visible = true;
+                treeView1.AutoScrollOffset = TreeviewScrolPos;
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            if (oneChapters==null||oneChapters.Length==0) return;
+            foreach(var item in oneChapters)
+            {
+                treeView1.Nodes.Add(item.Title.TrimStart());
+            }
+        }
+
+        private void treeView1_Click(object sender, EventArgs e)
+        {
+            if (treeView1.SelectedNode == null) return;
+            GetNovel(treeView1.SelectedNode.Index);
+            vScrollBar1.Value = 0;
+            Invalidate();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Invoke(new Action(()=>
+            {
+                FileDialog fdiag = new OpenFileDialog();
+                fdiag.Filter = "文本文件|*.txt";
+                fdiag.Title = "打开文件";
+                if(fdiag.ShowDialog()==DialogResult.OK)
+                {
+                    oneChapters = NovelsParser.LocalNovelsParser.NovelsFile(fdiag.FileName).ToArray();
+                    treeView1.Nodes.Clear();
+                    foreach (var item in oneChapters)
+                    {
+                        treeView1.Nodes.Add(item.Title.TrimStart());
+                    }
+                    GetNovel(0);
+                    Invalidate();
+                }
+                fdiag.Dispose();
+                GC.Collect(0);
+            }));
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mous_Y == 0) return;
+            int value1 = e.Y - Location.Y;
+            int value2 = value1 - mous_Y;
+            value2 = value2/ScrollSpeed;
+            int scrol_Y = vScrollBar1.Value + value2;
+            if(scrol_Y>=0&&scrol_Y<vScrollBar1.Maximum)
+            {
+                vScrollBar1.Value = scrol_Y;
+            }
+
+
+        }
+
+        int mous_Y;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button==MouseButtons.Right)
+            {
+                mous_Y = e.Y - Location.Y;
+            }
+        }
+
+        #endregion
+        private void vScrollBar1_ValueChanged(object sender, EventArgs e)
+        {
+            var s = sender as VScrollBar;
+            float scrollY = s.Value;
+            ScrollToLocal(scrollY, true);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mous_Y = 0;
+        }
+    }
+}
